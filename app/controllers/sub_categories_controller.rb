ffc6f8f1e9384c2eb337e997b51393f98078630d
Peter Phillips <peter.phillips@rtslabs.com>
2018-11-20 12:01:41 -0500

    insert sub category inline

--- app/controllers/sub_categories_controller.rb
+++ app/controllers/sub_categories_controller.rb
@@ -7,9 +7,7 @@ class SubCategoriesController < ApplicationController
   def create
     @category = Category.find(params[:category_id])
     @sub_category = @category.sub_categories.build
-    if @sub_category.update_attributes(sub_category_params)
-      flash.now[:notice] = "Sub Category created successfully."
-    else
+    unless @sub_category.update_attributes(sub_category_params)
       flash.now[:alert] = @sub_category.errors.full_messages.join(", ")
     end
   end
