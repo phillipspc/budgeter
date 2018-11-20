33c69d0b86ece05e39718487a8f4e6ae0ea00ed5
Peter Phillips <peter.phillips@rtslabs.com>
2018-11-20 11:52:08 -0500

    add sub categories with notifications

---
+++ app/controllers/sub_categories_controller.rb
@@ -0,0 +1,22 @@
+class SubCategoriesController < ApplicationController
+  def new
+    @category = Category.find(params[:category_id])
+    @sub_category = SubCategory.new
+  end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @sub_category = @category.sub_categories.build
+    if @sub_category.update_attributes(sub_category_params)
+      flash.now[:notice] = "Sub Category created successfully."
+    else
+      flash.now[:alert] = @sub_category.errors.full_messages.join(", ")
+    end
+  end
+
+  private
+
+    def sub_category_params
+      params.require(:sub_category).permit(:name)
+    end
+end
--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -1,4 +1,6 @@
 class SubCategory < ApplicationRecord
   belongs_to :category
   has_many :transactions
+
+  validates_uniqueness_of :name, scope: :category_id
 end
