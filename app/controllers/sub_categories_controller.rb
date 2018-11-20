f9d26f4ff2842382a55abafab443cd5feca79b07
Peter Phillips <peter.phillips@rtslabs.com>
2018-11-20 12:15:08 -0500

    edit sub categories

--- app/controllers/sub_categories_controller.rb
+++ app/controllers/sub_categories_controller.rb
@@ -2,11 +2,26 @@ class SubCategoriesController < ApplicationController
   def new
     @category = Category.find(params[:category_id])
     @sub_category = SubCategory.new
+    @url = category_sub_categories_path(@category)
   end
 
   def create
     @category = Category.find(params[:category_id])
     @sub_category = @category.sub_categories.build
+    unless @sub_category.update_attributes(sub_category_params)
+      flash.now[:alert] = @sub_category.errors.full_messages.join(", ")
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:category_id])
+    @sub_category = SubCategory.find(params[:id])
+    @url = category_sub_category_path(@category, @sub_category)
+  end
+
+  def update
+    @category = Category.find(params[:category_id])
+    @sub_category = @category.sub_categories.where(id: params[:id]).first
     unless @sub_category.update_attributes(sub_category_params)
       flash.now[:alert] = @sub_category.errors.full_messages.join(", ")
     end
