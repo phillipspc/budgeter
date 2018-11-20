04d15f4082ca601b4de36410924d0303a8aca691
Peter Phillips <peter.phillips@rtslabs.com>
2018-11-20 16:42:50 -0500

    crud categories

--- app/controllers/sub_categories_controller.rb
+++ app/controllers/sub_categories_controller.rb
@@ -32,7 +32,7 @@ class SubCategoriesController < ApplicationController
     sub_category = category.sub_categories.where(id: params[:id]).first
     @sub_category_id = sub_category&.id
     unless sub_category && sub_category.destroy
-      flash.now[:alert] = "Unable to delete the requrested Sub Category"
+      flash.now[:alert] = "Unable to delete the requested Sub Category"
     end
   end
 
