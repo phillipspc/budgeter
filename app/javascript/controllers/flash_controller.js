19d9a465431c157ff1898e142e9e77ef2b4135d5
Peter Phillips <peter.phillips@rtslabs.com>
2019-01-17 11:35:53 -0500

    working commit

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,24 +1,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["message", "style"]
-
-  connect() {
-    this.styleTargets.forEach((el, i) => {
-      el.classList.add(this.styleClass)
-    })
-  }
-
   close(event) {
-    this.messageTarget.remove()
-  }
-
-  get styleClass() {
-    const type = this.data.get("type")
-    if (type === "notice") {
-      return "is-success"
-    } else if (type === "alert") {
-      return "is-link"
-    }
+    this.element.remove()
   }
 }
