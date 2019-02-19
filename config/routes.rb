5837b81bccb71cc728b585d7b1f0e9e066551442
Peter Phillips <peter.phillips@rtslabs.com>
2019-02-19 12:10:25 -0500

    prevent deletion of sub categories, modal for reassigment

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
   resources :transactions
   get "/recurring", to: "transactions#recurring", as: :recurring
   resources :categories
-  resources :sub_categories
+  resources :sub_categories do
+    post "update_transactions_and_destroy", to: "sub_categories#update_transactions_and_destroy", on: :member
+  end
 end
