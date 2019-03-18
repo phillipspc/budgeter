0b4bf7594f5d7be0e975e788c8282483c20f9f60
Peter Phillips <peter.phillips@rtslabs.com>
2019-03-18 15:41:26 -0400

    handle recurring option for imported transactions, ensure not recurring

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -6,7 +6,12 @@ class Transaction < ApplicationRecord
   validates_presence_of :name, :amount
   validates_presence_of :date, unless: Proc.new { |transaction| transaction.recurring }
   validates_uniqueness_of :plaid_transaction_id, scope: :user_id
+  validates_inclusion_of :recurring, in: [false], if: :imported?
 
   scope :by_month, -> (month) { where(date: month.to_datetime..month.to_datetime.end_of_month) }
   scope :recurring, -> { where(recurring: true) }
+
+  def imported?
+    !!plaid_transaction_id
+  end
 end
