d16658b617c32b0be2faaea8f02a6248d0a54b74
Peter Phillips <peter.phillips@rtslabs.com>
2019-04-05 14:49:14 -0400

    remove console log

--- app/javascript/controllers/switch_controller.js
+++ app/javascript/controllers/switch_controller.js
@@ -5,10 +5,7 @@ export default class extends Controller {
     Rails.ajax({
       url: this.data.get("url"),
       type: "PATCH",
-      data: this.data.get("patch-data"),
-      success: function(data) {
-        console.log(data);
-      }
+      data: this.data.get("patch-data")
     });
   }
 }
