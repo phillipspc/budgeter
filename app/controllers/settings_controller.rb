4d752f2d31c7c1f81a75a3493aff875007dcd601
Peter Phillips <peter.phillips@rtslabs.com>
2019-04-05 13:08:37 -0400

    logic to update include_recurring setting

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -5,4 +5,9 @@ class SettingsController < ApplicationController
   def edit
     @users = current_user.users.order("email")
   end
+
+  def update
+    current_user.update_attributes(include_recurring: params[:include_recurring])
+    redirect_to params[:redirect_url]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     root to: 'devise/sessions#new'
   end
 
-  resource :settings, only: :edit
+  resource :settings, only: [:edit, :update]
 
   resources :users, only: :destroy
   resources :transactions
