d2827ba35f26358b80a8919f125468f991858e96
Peter Phillips <peter.phillips@rtslabs.com>
2019-04-24 17:26:33 -0400

    integrate sentry raven

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
     super
 
     @manager = current_user.is_manager? ? current_user : current_user.manager
+    set_raven_context
   end
 
   def confirm_manager
@@ -15,4 +16,11 @@ class ApplicationController < ActionController::Base
   def set_month
     @month = params[:month] || Time.current.strftime("%B %Y")
   end
+
+  private
+
+    def set_raven_context
+      Raven.user_context(id: current_user.id)
+      Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+    end
 end
