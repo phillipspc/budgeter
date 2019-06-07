9cde5c91f026bd8381b2e09b68f3ec09a925fb12
Peter Phillips <peter.phillips@rtslabs.com>
2019-06-07 14:25:59 -0400

    dont send email if no pending transactions

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,8 +4,9 @@ class NotificationMailer < ApplicationMailer
   def pending_imports
     @user = params[:user]
 
-    @this_months_imports = @user.manager.plaid_imports.where(month: params[:month])
-    @previous_months_imports = @user.manager.plaid_imports.where(month: params[:previous_month])
+    @this_months_pending = @user.manager.plaid_imports.where(month: params[:month]).map(&:pending_count).sum
+    @previous_months_pending = @user.manager.plaid_imports.where(month: params[:previous_month]).map(&:pending_count).sum
+    return unless @this_months_pending.positive? || @previous_months_pending.positive?
     @previous_month = params[:previous_month]
     mail(to: @user.email, subject: "You have transactions waiting to be imported")
   end
