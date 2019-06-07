b0674d655b92a0567c66a7facae42cd179b7bfa1
Peter Phillips <peter.phillips@rtslabs.com>
2019-06-07 13:42:46 -0400

    tweak mailer view

--- app/controllers/plaid/base_controller.rb
+++ app/controllers/plaid/base_controller.rb
@@ -9,9 +9,12 @@ class Plaid::BaseController < ApplicationController
     end
 
     def set_client
-      @client = Plaid::Client.new(env: Rails.application.credentials[Rails.env.to_sym][:plaid_env],
-                                  client_id: Rails.application.credentials[Rails.env.to_sym][:plaid_client_id],
-                                  secret: Rails.application.credentials[Rails.env.to_sym][:plaid_secret],
-                                  public_key: Rails.application.credentials[Rails.env.to_sym][:plaid_public_key])
+      # for admin only, use production Plaid client in development
+      rails_env = user.email == Rails.application.credentials[Rails.env.to_sym][:admin_email] ? :production : Rails.env.to_sym
+
+      @client = Plaid::Client.new(env: Rails.application.credentials[rails_env][:plaid_env],
+                                  client_id: Rails.application.credentials[rails_env][:plaid_client_id],
+                                  secret: Rails.application.credentials[rails_env][:plaid_secret],
+                                  public_key: Rails.application.credentials[rails_env][:plaid_public_key])
     end
 end
