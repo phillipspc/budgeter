47586e78435f5d535eca822c09c707145a7d1cfa
Peter Phillips <peter.phillips@rtslabs.com>
2019-10-09 18:21:10 -0400

    mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,21 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp-relay.sendinblue.com",
+    :port                 => 587,
+    :user_name            => Rails.application.credentials[Rails.env.to_sym][:sendinblue_email],
+    :password             => Rails.application.credentials[Rails.env.to_sym][:sendinblue_password],
+    :authentication       => "login",
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.default_url_options = { host: 'mybudgeterapp.herokuapp.com' }
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
